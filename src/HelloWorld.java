0aeb1ef7c3d05b8542b0d9b6fc652ab4f45a7d0d
Administrator <Administrator@MSDN-SPECIAL>
2019-01-06 17:10:56 +0900

    Hello World 소스 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("Nice to meet you");
+		System.out.println("Nice to meet you");
+		System.out.println("Thank you");
 	}
 	
 }
